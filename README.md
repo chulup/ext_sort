dd4881cbd2d846cc0b806ca6248d1d0b57d095d5
Michael Shulbaev <chulupey@gmail.com>
2018-10-04 16:39:23 +0700

    ext_sort crashes on dma_read

---
+++ README.md
@@ -0,0 +1,1 @@
+Do not forget to add environment variable PKG_CONFIG_PATH=<path_to_seastar>/build/release to CMake invocation!
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,45 @@
 
 #include <core/app-template.hh>
 #include <core/reactor.hh>
+#include <core/thread.hh>
+#include <core/file.hh>
+#include <core/sstring.hh>
+#include <core/print.hh>
 
-int main() {
-    std::cout << "ARGH" << std::endl;
+using namespace seastar;
+
+const size_t BLOCK = 4096;
+const size_t ALIGNMENT = 4096;
+
+future<> read_data(const sstring &filename) {
+    return open_file_dma(filename, open_flags::ro)
+        .then([](file f) {
+            f.size().then([&](auto fsize) {
+                return f.dma_read<uint8_t>(0, 4096).then([](auto temp_buf){
+
+                });
+            });
+        });
+}
+
+int main(int argc, char *argv[]) {
+    seastar::app_template app;
+    namespace bpo = boost::program_options;
+
+    app.add_positional_options({
+        {
+            "filename",
+            bpo::value<sstring>()->required(),
+            "file to sort",
+            1
+        }
+    });
+    app.run(argc, argv, [&] {
+        auto &opts = app.configuration();
+        auto &filename = opts["filename"].as<sstring>();
+
+        return read_data(filename);
+    });
+
     return 0;
 }
