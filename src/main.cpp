24791e3718371a9fde3c8388106e519df1814c29
Michael Shulbaev <chulupey@gmail.com>
2018-10-04 21:26:35 +0700

    File reading test - works with stream, doesn't work with DMA read

--- src/main.cpp
+++ src/main.cpp
@@ -10,43 +10,47 @@
 
 using namespace seastar;
 
-//const size_t BLOCK = 4096;
-//const size_t ALIGNMENT = 4096;
+const size_t BLOCK = 4096;
+const size_t ALIGNMENT = 4096;
 
-future<> read_data(const sstring &filename) {
-    print("before open: %s\n", filename);
-    return open_file_dma(filename, open_flags::ro)
-        .then([filename](file f) {
-            print("after open: %s\n", filename);
-            f.size().then([f = std::move(f), &filename] (auto fsize) mutable {
-                print("File \"%s\" of size %ul\n", filename, fsize);
-                print("Read: %d, write: %d, memory: %d\n",
-                    f.disk_read_dma_alignment(),
-                    f.disk_write_dma_alignment(),
-                    f.memory_dma_alignment());
+#define USE_DMA_READ 0
 
-#if 1
-                return f.template dma_read_exactly<char>(0, 4096).then([](auto buf) {
-                    print("Read %d bytes: %X%X%X%X\nsizeof(tmp_buf[0]) = %d\n",
-                                buf.size(),
-                                buf[0], buf[1], buf[2], buf[3],
-                                sizeof(buf[1]));
-                    return make_ready_future<>();
-                }).finally([&f]{});
+future<> read_from_file(file &f, size_t pos, size_t length, char *out_buf) {
+#if USE_DMA_READ
+    return f.dma_read(pos, out_buf,  length).then([&](size_t read) {
+        BOOST_ASSERT(read == length);
+        return make_ready_future<>();
+    });
 #else
-                return do_with(input_stream<char>(make_file_input_stream(std::move(f))),
-                    [](input_stream<char>& is) -> future<> {
-                        return is.read_up_to(4096).then([](auto tmp_buf) {
-                            print("Read %d bytes: %X%X%X%X\nsizeof(tmp_buf[0]) = %d\n",
-                                tmp_buf.size(),
-                                tmp_buf[0], tmp_buf[1], tmp_buf[2], tmp_buf[3],
-                                sizeof(tmp_buf[1]));
-                            return make_ready_future<>();
-                        });
-                });
-#endif
+    return do_with(input_stream<char>(make_file_input_stream(f, pos, length)),
+        [&](input_stream<char>& is) -> future<> {
+            return is.read_exactly(length).then([&](temporary_buffer<char> buf) {
+                std::memcpy(out_buf, buf.begin(), length);
             });
         });
+#endif
+}
+
+future<> read_data(const sstring &filename) {
+//    print("before open: %s\n", filename);
+    return open_file_dma(filename, open_flags::ro).then([filename](file f) {
+//            print("after open: %s\n", filename);
+//            f.size().then([f = std::move(f)/*, &filename*/] (auto fsize) mutable {
+//                print("File \"%s\" of size %ul\n", filename, fsize);
+//                print("Read: %d, write: %d, memory: %d\n",
+//                    f.disk_read_dma_alignment(),
+//                    f.disk_write_dma_alignment(),
+//                    f.memory_dma_alignment());
+        const size_t size = BLOCK;
+        auto bufptr = allocate_aligned_buffer<char>(size, ALIGNMENT);
+        auto buf = bufptr.get();
+        return read_from_file(f, 0, size, buf).then([bufptr = std::move(bufptr)] () {
+            auto buf = bufptr.get();
+            print("read data: %X%X%X%X\n",
+                buf[0], buf[1], buf[2], buf[3]);
+            return make_ready_future<>();
+    });
+    });
 }
 
 int main(int argc, char *argv[]) {
