0cee59fd77179e3b7fa986f9fa8fea4a13bac6ed
Michael Shulbaev <chulupey@gmail.com>
2018-10-08 10:13:03 +0700

    Sort blocks

--- src/main.cpp
+++ src/main.cpp
@@ -13,44 +13,34 @@ using namespace seastar;
 
 const size_t RECORD_SIZE = 16;
 const size_t IO_BLOCK_SIZE = 4096;
-const size_t ALIGNMENT = 4096;
-
-#define USE_DMA_READ 0
-
-future<> read_from_file(file &f, uint64_t pos, size_t length, char *out_buf) {
-#if USE_DMA_READ
-//    return f.dma_read(pos, out_buf,  length).then([&](size_t read) {
-//        BOOST_ASSERT(read == length);
-//        return make_ready_future<>();
-//    });
-    return f.template dma_read_bulk<char>(pos, length).then([out_buf, length] (temporary_buffer<char> buf) {
-        std::memcpy(out_buf, buf.begin(), std::min(buf.size(), length));
-    });
-#else
-    return do_with(input_stream<char>(make_file_input_stream(f, pos, length)),
-        [out_buf, length](input_stream<char>& is) -> future<> {
-            return is.read_exactly(length).then([out_buf, length](temporary_buffer<char> buf) {
-                std::memcpy(out_buf, buf.begin(), length);
-            });
-        });
-#endif
-}
 
 template<size_t RECORD_SIZE>
 void sort_records(void *data, size_t len) {
-    BOOST_ASSERT(len % RECORD_SIZE == 0);
-
     struct record_t {
         uint8_t data[RECORD_SIZE];
     };
 
+    BOOST_ASSERT(len % sizeof(record_t) == 0);
+    BOOST_ASSERT(sizeof(record_t) == RECORD_SIZE);
+    const size_t count = len / sizeof(record_t);
     record_t *records = reinterpret_cast<record_t*>(data);
-    const size_t count = len / RECORD_SIZE;
+//    std::sort(records, records + count-1, [] (const record_t& left, const record_t &right) {
+//        return std::memcmp(left.data, right.data, RECORD_SIZE) < 0;
+//    });
 
-    std::sort(records, &records[count], [] (const record_t& left, const record_t &right) {
-        return std::memcmp(left.data, right.data, RECORD_SIZE) < 0;
+    std::vector<size_t> indexes(count);
+    std::iota(indexes.begin(), indexes.end(), 0);
+    std::sort(indexes.begin(), indexes.end(), [&] (size_t left, size_t right) {
+        return std::memcmp(records[left].data, records[right].data, sizeof(record_t)) < 0;
     });
+
+    std::vector<record_t> tmp(indexes.size());
+    for (size_t i = 0; i < tmp.size(); i++) {
+        tmp[i] = records[indexes[i]];
+    }
+    std::memcpy(data, tmp.data(), tmp.size() * sizeof(tmp[0]));
 }
+
 
 int main(int argc, char *argv[]) {
     seastar::app_template app;
@@ -69,46 +59,47 @@ int main(int argc, char *argv[]) {
         auto &opts = app.configuration();
         const auto &filename = opts["filename"].as<sstring>();
 
-        return async([&filename] {
+        return async([&filename] () {
+
+            file f = open_file_dma(filename, open_flags::rw).get0();
+            uint64_t fsize = f.size().get0();
+            if (fsize % RECORD_SIZE != 0) {
+                throw std::runtime_error("File size is not a multiple of RECORD_SIZE");
+            }
             const size_t sort_concurrency = seastar::smp::count;
             const size_t sort_block_size = IO_BLOCK_SIZE; // while RECORD_SIZE is small
 
-            file f = open_file_dma(filename, open_flags::rw).get0();
-
             parallel_for_each(
                     boost::irange<size_t>(0, sort_concurrency),
-                    [&f] (size_t id) {
+                    [&f, fsize, sort_concurrency] (size_t id) -> future<> {
+                size_t start = sort_block_size * id;
+
                 auto buf = temporary_buffer<uint8_t>::aligned(f.memory_dma_alignment(), sort_block_size);
-                const size_t position = sort_block_size * id;
+                // Calculate positions current execution thread will sort;
+                // currently for 4 threads it's 0, 4, 8...; 1, 5, 9...; 2, 6, 10...; 3, 7, 11...
+                auto positions = boost::irange<uint64_t>(start, fsize, sort_block_size * sort_concurrency);
 
-                return f.dma_read(position, buf.get_write(), buf.size())
-                        .then([&buf, position, &f] (size_t read_bytes) mutable {
-                    sort_records<RECORD_SIZE>(buf.get_write(), read_bytes);
+                do_for_each(positions,
+                        [&buf, &f] (uint64_t position) mutable -> future<> {
+                    return f.dma_read(position, buf.get_write(), buf.size())
+                            .then([&buf, position, &f] (size_t read_bytes) mutable -> future<>
+                    {
+                        // read_bytes could be less than buf.size() when in the end of file;
+                        sort_records<RECORD_SIZE>(buf.get_write(), read_bytes);
 
-                    return f.dma_write(position, buf.get(), read_bytes)
-                        .then([read_bytes] (auto write_bytes) {
-                            if (read_bytes != write_bytes) {
-                                return make_exception_future(std::runtime_error("Failed to write all the data"));
-                            }
-                            return make_ready_future();
-                        });
-                }).get0();
+                        return f.dma_write(position, buf.get(), read_bytes)
+                            .then([read_bytes] (auto write_bytes) {
+                                if (read_bytes != write_bytes) {
+                                    return make_exception_future(std::runtime_error("Failed to write all the data"));
+                                }
+                                return make_ready_future();
+                            });
+                    });
+                }).get();
+
+                return make_ready_future();
             }).get();
         });
-
-//        return file_size(filename).then([&filename, &bufptr] (uint64_t fsize) {
-//            return open_file_dma(filename, open_flags::rw).then([/*fsize, */&bufptr](file f) {
-//                print("File has been opened\n");
-
-//                size_t pos = 0;
-
-//                return read_from_file(f, pos, bufptr.size(), bufptr.get_write())/*.then([&bufptr] () mutable {
-//                    const auto buf = bufptr.begin();
-//                    print("Read data: %X %X %X %X\n", buf[0], buf[1], buf[2], buf[3]);
-//                    return sort_records(bufptr);
-//                })*/;
-//            });
-//        });
     });
 
     return 0;
