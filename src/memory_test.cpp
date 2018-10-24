6733be0caac8510df586e7d60d577f2745bba88b
Michael Shulbaev <chulupey@gmail.com>
2018-10-24 15:55:10 +0700

    Change memory test: allocate multiple buffers

--- src/memory_test.cpp
+++ src/memory_test.cpp
@@ -13,35 +13,43 @@ int main(int argc, char *argv[]) {
         const auto stats = seastar::memory::stats();
         const auto total_free = stats.free_memory();
         const auto thread_count = seastar::smp::count;
-        seastar::print("free mem: %.2f G, thread_count: %u\n", total_free/GB, thread_count);
-        seastar::print("memory per thread: %.2f G\n", (total_free/thread_count)/GB);
+        seastar::print("free_memory: %.2f G, smp::count: %u\n", total_free/GB, thread_count);
+        seastar::print("free_memory/smp::count : %.2f G\n", (total_free/thread_count)/GB);
 
         const uint64_t increment = 512 * 1024 * 1024; // 512M
         const uint64_t decrement = 64 * 1024 * 1024; // 64M
-        uint64_t current = increment;
 
-        seastar::print("Allocating buffers starting from size %.2f G with increment %.2f G\n", current/GB, increment/GB);
-        while (true) {
-            try {
-                auto buf = seastar::temporary_buffer<char>::aligned(4096, current);
-                current += increment;
-            } catch (const std::bad_alloc &) {
-                seastar::print("Got bad_alloc on size %.2f G\n", current/GB);
-                break;
+        std::vector<seastar::temporary_buffer<char>> v(8);
+        uint64_t total_size = 0;
+
+        for (unsigned i = 0; i < v.size(); i++) {
+            seastar::print("Allocating buffer #%d... ", i);
+            seastar::temporary_buffer<char> buffer;
+            uint64_t current = increment;
+            while (true) {
+                try {
+                    buffer = seastar::temporary_buffer<char>::aligned(4096, current);
+                    current += increment;
+                    std::move(buffer);
+                } catch (const std::bad_alloc &) {
+                    break;
+                }
             }
+
+            while (true) {
+                try {
+                    buffer = seastar::temporary_buffer<char>::aligned(4096, current);
+                    break;
+                } catch (const std::bad_alloc &) {
+                    current -= decrement;
+                }
+            }
+            v[i] = std::move(buffer);
+            total_size += v[i].size();
+            seastar::print("maximum size is %.2f G\n", current/GB);
         }
 
-        seastar::print("Allocating buffers starting from size %.2f G with decrement %.2f G\n", current/GB, decrement/GB);
-        while (true) {
-            try {
-                auto buf = seastar::temporary_buffer<char>::aligned(4096, current);
-                seastar::print("Successfully allocated buffer of size %.2f G\n", current/GB);
-                break;
-            } catch (const std::bad_alloc &) {
-                current -= decrement;
-            }
-        }
-        seastar::print("Maximum buffer size is %.2f G\n", current/GB);
+        seastar::print("Total memory allocated in %d buffers is %.2f G\n", v.size(), total_size/GB);
 
         return seastar::make_ready_future<>();
     });
